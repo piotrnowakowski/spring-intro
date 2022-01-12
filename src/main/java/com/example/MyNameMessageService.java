324fd671c837705f5f8979279ae170b558fef554
piotrnowakowski <61783713+piotrnowakowski@users.noreply.github.com>
2022-01-12 16:43:40 +0100

    11

--- src/main/java/com/example/MyNameMessageService.java
+++ src/main/java/com/example/MyNameMessageService.java
@@ -1,4 +1,11 @@
 package com.example;
 
 public class MyNameMessageService implements MessageService {
+
+    @Override
+    public String getMessage() {
+        String expectedMessage = "Piotr Nowakowski";
+        return expectedMessage;
+    }
+
 }
---
+++ src/main/java/com/example/RandomTextMessageService.java
@@ -0,0 +1,28 @@
+package com.example;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@Component("randomMessageService")
+public class RandomTextMessageService implements MessageService{
+    ArrayList<String> lista = new ArrayList<String>(){{
+        add("m1");
+        add("m2");
+        add("m3");
+        add("m4");
+        add("m5");
+        add("m6");
+        add("m7");
+        add("m8");
+        add("m9");
+        add("m10");
+    }};
+
+    @Override
+    public String getMessage() {
+        int liczba = new Random().nextInt(9 + 1);
+        return this.lista.get(liczba);
+    }
+}
